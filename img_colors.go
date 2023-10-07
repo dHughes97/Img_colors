57a7e9847b99108e3c0b57171863b1e938d48596
GoldenKunai <dhhollywood273@gmail.com>
2023-10-06 22:13:41 -0500

    updated

--- img_colors.go
+++ img_colors.go
@@ -3,13 +3,15 @@ package Img_colors
 import (
 	"fmt"
 	"image"
+	_ "image/jpeg"
 	_ "image/png" // import this package to decode PNGs
+
 	"os"
 )
 
 // adding comments
 func ImgColors() {
-	reader, err := os.Open("image.png")
+	reader, err := os.Open("colors.jpg")
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
@@ -24,12 +26,26 @@ func ImgColors() {
 
 	bounds := img.Bounds()
 	width, height := bounds.Max.X, bounds.Max.Y
+	// Open output file for writing
+	file, errs := os.Create("colors_pixel_counts.txt")
+	if errs != nil {
+		fmt.Println("Failed to create file:", errs)
+		return
+	}
+	defer file.Close()
 
+	fmt.Println("Processing Pixel Colors")
+
+	// Prints out results of the decoder
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
 			color := img.At(x, y)
 			r, g, b, _ := color.RGBA()
-			fmt.Printf("Pixel at (%d, %d) - R: %d, G: %d, B: %d\n", x, y, r>>8, g>>8, b>>8)
+
+			values := fmt.Sprintf("Pixel at (%d, %d) - R: %d, G: %d, B: %d\n", x, y, r>>8, g>>8, b>>8)
+			file.WriteString(values)
+
+			values = ""
 		}
 	}
 }
